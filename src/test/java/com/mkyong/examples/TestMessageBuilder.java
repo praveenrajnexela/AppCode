88c245254549448ed62d206f4770f788f5cc9040
SKaliyamoorthy <SKaliyamoorthy@lason.com>
2021-06-08 02:51:14 -0400

    testing

--- src/test/java/com/mkyong/examples/TestMessageBuilder.java
+++ src/test/java/com/mkyong/examples/TestMessageBuilder.java
@@ -6,13 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestMessageBuilder {
 
-     /*@Test
+     @Test
    public void testNameMkyong() {
 
         MessageBuilder obj = new MessageBuilder();
         assertEquals("Hello mkyong", obj.getMessage("mkyong"));
 
-    }*/
+    }
 
  @Test
 
